c07ea2be0f916a4a77198e33c7b95b8e41175446
ntynhi <N20681@eon.com>
2023-09-18 13:50:44 +0700

    Hook tutorial  6 7 8

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import HookCounterFour from './hook_components/HookCounterFour';
+import HookCounterOne from './hook_components/HookCounterOne';
+import HookMouse from './hook_components/HookMouse';
 function App() {
   return (
     <div className="App">
-      <HookCounterFour></HookCounterFour>
+      <HookMouse></HookMouse>
     </div>
   );
 }
---
+++ src/hook_components/HookMouse.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+
+function HookMouse() {
+    const [x, setX] = useState(0)
+    const [y, setY] = useState(0)
+
+    const logMousePosition = (e) => {
+        console.log('Move event')
+        setX(e.clientX)
+        setY(e.clientY)
+    }
+
+    useEffect(()=>{
+        console.log("useEffect call")
+        window.addEventListener('mousemove', logMousePosition)
+    }, [])
+    return (
+        <div>
+            Hook X {x}, Y {y}
+        </div>
+    )
+}
+
+export default HookMouse
