2b4ddbff962f0e293c8764c0ed8dea28e9f5f19e
ntynhi <N20681@eon.com>
2023-09-20 18:58:24 +0700

    react axios post and get

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
 import ComponentA from './components/ComponentA';
+import PostList from './components/PostList';
 import { UserProvider } from './components/UserContext';
 import HookCounterFour from './hook_components/HookCounterFour';
 import HookCounterOne from './hook_components/HookCounterOne';
 import HookMouse from './hook_components/HookMouse';
 function App() {
   return (
-    <div className="App">
-      <UserProvider value = "context test">
-        <ComponentA></ComponentA>
-      </UserProvider>
+    <div className="App">  
+      <PostList/>
     </div>
   );
 }
