2495393e2a50ea542eb863dc6e81fa0a9ff7b9f2
ntynhi <N20681@eon.com>
2023-09-20 12:59:58 +0700

    react context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import ComponentA from './components/ComponentA';
+import { UserProvider } from './components/UserContext';
 import HookCounterFour from './hook_components/HookCounterFour';
 import HookCounterOne from './hook_components/HookCounterOne';
 import HookMouse from './hook_components/HookMouse';
 function App() {
   return (
     <div className="App">
-      <HookMouse></HookMouse>
+      <UserProvider value = "context test">
+        <ComponentA></ComponentA>
+      </UserProvider>
     </div>
   );
 }
