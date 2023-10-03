37ee342889a97512e8256c3abcf613e82ba9a496
ntynhi <N20681@eon.com>
2023-10-03 13:35:07 +0700

    [render] useState

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import './App.css';
 import React from 'react';
-import ParentComponent from './hook_components/callback/ParentComponent';
-import Counter from './hook_components/memo/Counter';
-import FocusInput from './hook_components/ref/FocusInput';
-import DocTitleOne from './hook_components/custom_hook/DocTitleOne';
-import DocTitleTwo from './hook_components/custom_hook/DocTitleOne';
-import CounterOne from './hook_components/custom_hook/CounterOne';
-import CounterTwo from './hook_components/custom_hook/CounterTwo';
-import UserForm from './hook_components/custom_hook/UserForm';
+import UseState from './render/component/UseState';
 
 
 function App() {
   
   return (
     <div className="App">
-      <UserForm></UserForm>
+      <UseState></UseState>
     </div>
   );
 }
