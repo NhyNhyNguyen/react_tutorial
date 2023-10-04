2c564365c0d8e7558e62078e6200077dad849d21
ntynhi <N20681@eon.com>
2023-10-04 13:37:45 +0700

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import React from 'react';
 import UseState from './render/component/UseState';
+import UseReducer from './render/component/UseReducer/UseReducer';
 
 
 function App() {
   
   return (
     <div className="App">
-      <UseState></UseState>
+      <UseReducer></UseReducer>
     </div>
   );
 }
